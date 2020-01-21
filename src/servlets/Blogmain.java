7867cdf1eb2d07e73b72af76ddde87b952df31f8
ValeKI <44066795+valeki@users.noreply.github.com>
2020-01-21 12:33:02 +0100

    commenti e articoli

--- src/servlets/Blogmain.java
+++ src/servlets/Blogmain.java
@@ -6,11 +6,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 @WebServlet(name = "Blogmain")
 public class Blogmain extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String button = request.getParameter("blogButton");
+        if(Pattern.matches("nuovoArticolo", button))
+        {
+            System.out.println("NUOVO ARTICOLO");
+        }
+        else if(Pattern.matches("miPiace.+", button))
+        {
+            System.out.println("MI PIACE");
+        }
+        else if(Pattern.matches("nuovoCommento.+", button))
+        {
+            System.out.println("NUOVO COMMENTO");
+        }
+        System.out.println("VOLEVI: " + button);
+        request.getRequestDispatcher("blogmain.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
